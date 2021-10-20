fac7b6f75f32e9d389ae7d8a0d586379ed6e77ad
Pablo Estrella <almendaris123@gmail.com>
2021-10-20 00:39:52 -0500

    se reparó el borrar

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
 require('dotenv').config();
 
 const { dbConnection } = require('./database/config');
@@ -10,6 +11,7 @@ app.use(cors());
 app.use( bodyParser.urlencoded( { extended : true }));
 
 app.use('/user', require('./routes/user'));
+app.use(morgan('dev'))
 
 app.listen( process.env.PORT || 3000, () => {
     const port = process.env.PORT || 3000;
